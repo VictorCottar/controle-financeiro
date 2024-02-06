4820658d44ee9fb2ec0160a4a929db2411f064a9
VictorCottar <victorcottarms@outlook.com>
2024-02-06 17:18:48 -0300

    Novo id para o input

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 let balanceValue = 0;
 
 function deposit() {
-    const valueDeposit = parseFloat(document.getElementById('deposit__amount').value);
+    const valueDeposit = parseFloat(document.getElementById('deposit__amount_input').value);
 
     if (valueDeposit > 0) {
         balanceValue += valueDeposit;
 
         document.getElementById('balance').innerText = balanceValue;
-        document.getElementById('deposit__amount').value = '';
+        document.getElementById('deposit__amount_input').value = '';
+        
+        armazenarValorDeposito();
     } else {
         alert('Digite um número válido para depósito!')
     }
@@ -26,3 +28,18 @@ function withdraw() {
         document.getElementById('withdraw__amount').value = ''
     }
 }
+
+function armazenarValorDeposito() {
+
+    //pego o valor digitado pelo usuário no input de deposito.
+    const valorDeposito = document.getElementById('deposit__amount_input').value;
+
+    //crio o obj passando o valor da chave com a variável que pega o input depósito.
+    const objDeposito = { valor: valorDeposito };
+
+    localStorage.setItem('deposito', JSON.stringify(objDeposito));
+
+    const lerDeposito = JSON.parse(localStorage.getItem('deposito'));
+
+    console.log(lerDeposito);
+};
